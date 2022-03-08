41223ba56e2c6fb2e0479d70ade0b1ecd48d2bcf
DTroczyk <DTroczyk@gmail.com>
2022-03-08 20:51:38 +0100

    feat: Buy button for courses

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import bemCssModules from 'bem-css-modules';
 
+import request from '../../helpers/request';
+import { StoreContext } from '../../store/StorePrivider';
+
 import { default as CourseStyles } from './Course.module.scss'
+import { useNavigate } from 'react-router-dom';
 
 const style = bemCssModules(CourseStyles);
 
-const Course = ({ authors, img, price, title}) => {
+const Course = ({ authors, id, isUserContext = false, img, price, title}) => {
+  const { user, setUser } = useContext(StoreContext)
+  const navigate = useNavigate();
+
   const allAuthors = authors.join(', ');
+  const isUserLogged = Boolean(user);
+
+  const handleOnClick = async () => {
+    try {
+      const { data, status } = await request.patch(
+        '/users',
+        {
+          login: user.login,
+          courseId: id
+        }
+      );
+      
+      if (status === 202) {
+        setUser(data.user);
+        navigate('/my-courses');
+      }
+
+    } catch (error) {
+      console.warn(error);
+    }
+  }
+
+  const shouldBeBuyButtonVisible = isUserLogged && !isUserContext;
 
   return (
     <li className={style()}>
@@ -15,6 +45,7 @@ const Course = ({ authors, img, price, title}) => {
         <img alt={title} className={style('image')} src={img}/>
         <p className={style('price')}>{`Koszt kursu: ${price}zł`}</p>
         <p className={style('authors')}>{`Autorzy kursu: ${allAuthors}`}</p>
+        {shouldBeBuyButtonVisible && <button onClick={handleOnClick}>Kup kurs</button>}
       </article>
     </li>
   )
